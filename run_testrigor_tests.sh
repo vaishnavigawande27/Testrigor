bd5ac573ee8a88f1063192b30ccfdd66b692852c
Vaishnavi Gawande <92210689+vaishnavigawande27@users.noreply.github.com>
2026-02-04 11:33:45 +0530

    run_testrigor_tests.sh

--- run_testrigor_tests.sh
+++ run_testrigor_tests.sh
@@ -2,8 +2,8 @@
 
 # Test parameters (replace with actual login details and URLs)
 WEBMAIL_URL="https://mail.microproindia.com/webmail/#sign-in"
-USER="${{ secrets.EMAIL_USERNAME }}"
-PASS="${{ secrets.EMAIL_PASSWORD }}"
+USER="$EMAIL_USERNAME"  # Correctly reference the environment variable
+PASS="$EMAIL_PASSWORD"  # Correctly reference the environment variable
 
 # Send a POST request to simulate login and get the response code
 response=$(curl -s -w "%{http_code}" -o /tmp/webmail_response.txt -X POST \
